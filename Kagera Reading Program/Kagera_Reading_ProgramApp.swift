89cb7b5df0afa3cb8e403d0857c8d3c26e3abc57
Steve Clark <bukoba.steve@gmail.com>
2024-11-11 10:10:19 +1100

    Initial Commit

---
+++ Kagera Reading Program/Kagera_Reading_ProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kagera_Reading_ProgramApp.swift
+//  Kagera Reading Program
+//
+//  Created by Stephen Clark on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kagera_Reading_ProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
